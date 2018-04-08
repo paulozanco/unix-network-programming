7dcd8956271ca94838164e2a56ec055e6b6be134
Paulo Cesar Zanco <paulo.zanco@gmail.com>
2018-04-08 20:46:32 -0300

    add .gitignore

--- laboratory/daytimecpcli.c
+++ laboratory/daytimecpcli.c
@@ -1,6 +1,5 @@
 /* Copyright 2018 Paulo Zanco */
 
-#include <netinet/in.h>
 #include "./zanco.h"
 
 #define MAXLINE 4096 /* max text line length */
@@ -13,5 +12,18 @@ int main(int argc, char *argv[]) {
 
   if (argc != 2)
     err_quit("usage: prog <IPaddress>");
-  return 0;
+
+  if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    err_sys("socket error");
+
+  bzero(&servaddr, sizeof(servaddr));
+
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(13);
+
+  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    err_quit("inet_pton error for %s", argv[1]);
+  
+  exit(0);
 }
+
--- laboratory/zanco.h
+++ laboratory/zanco.h
@@ -7,10 +7,18 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <strings.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #define MAXLINE 4096
 
 void err_quit(const char *, ...);
+void err_sys(const char*, ...);
+
 
 #endif  // LABORATORY_ZANCO_H_
 
